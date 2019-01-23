69c095f6f5363918b06f2a3e50471490b93f64ef
Dana Simmons <dsanalog@gmail.com>
2019-01-23 15:41:55 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,44 @@
+def languages()
+  languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+end
+
+def children(parent)
+  parent.values()
+end
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  languages.each do |_key value|
+    
 end
